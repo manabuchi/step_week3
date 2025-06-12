31542e23a3742a34257a61ab32a307f87e0b2ee3
manabuchi <dream.music.0330@gmail.com>
2025-06-12 22:22:15 +0900

    homework3

--- modularized_calculator.py
+++ modularized_calculator.py
@@ -15,7 +15,6 @@ def read_number(line, index):
     token = {'type': 'NUMBER', 'number': number}
     return token, index
 
-
 def read_plus(line, index):
     token = {'type': 'PLUS'}
     return token, index + 1
@@ -32,6 +31,14 @@ def read_divide(line, index):
     token = {'type' : 'DIVIDE'}
     return token, index + 1
 
+def read_startparen(line, index):
+    token = {'type' : 'STARTPAREN'}
+    return token, index + 1
+
+def read_endparen(line, index):
+    token = {'type' : 'ENDPAREN'}
+    return token, index + 1
+
 def tokenize(line):
     tokens = []
     index = 0
@@ -46,12 +53,34 @@ def tokenize(line):
             (token, index) = read_times(line, index)
         elif line[index] == '/':
             (token, index) = read_divide(line, index)
+        elif line[index] == '(':
+            (token, index) = read_startparen(line, index)
+        elif line[index] == ')':
+            (token, index) = read_endparen(line, index)        
         else:
             print('Invalid character found: ' + line[index])
             exit(1)
         tokens.append(token)
         # print("tokenized!")
     return tokens
+
+def evaluate_expression(tokens, start=0):
+    new_tokens = []
+    index = start
+    while index < len(tokens):
+        token = tokens[index]
+        if token['type'] == 'STARTPAREN':
+            # 再帰で内側の括弧を評価
+            sub_tokens, next_index = evaluate_expression(tokens, index + 1)
+            value = evaluate_plus_and_minus(evaluate_times_and_divide(sub_tokens))
+            new_tokens.append({'type': 'NUMBER', 'number': value})
+            index = next_index
+        elif token['type'] == 'ENDPAREN':
+            return new_tokens, index + 1
+        else:
+            new_tokens.append(token)
+            index += 1
+    return new_tokens, index
 
 def evaluate_times_and_divide(tokens):
     new_tokens = []
@@ -100,11 +129,13 @@ def evaluate_plus_and_minus(tokens):
 
 def test(line):
     tokens = tokenize(line)
-    first_answer = evaluate_times_and_divide(tokens)
-    if first_answer == None:
+    tokens = tokenize(line)
+    evaluated_tokens, _ = evaluate_expression(tokens)
+    second_answer = evaluate_times_and_divide(evaluated_tokens)
+    if second_answer is None:
         print("ZeroDivisionError")
-        return 
-    actual_answer = evaluate_plus_and_minus(first_answer)
+        return
+    actual_answer = evaluate_plus_and_minus(second_answer)
     expected_answer = eval(line)
     if abs(actual_answer - expected_answer) < 1e-8:
         print("PASS! (%s = %f)" % (line, expected_answer))
@@ -131,6 +162,19 @@ def run_test():
     test("3.0+4*2")
     test("3.0+4*2-1/5")
     test("1.0+2.1-3")
+    # with parenthesis
+    test("(1+2)")
+    test("(3+4)*2")
+    test("1+(2*3)")
+    test("((1+2)*3)")
+    test("4*(5+6)/2")
+    test("((1+1)+(2+2))")
+    test("((1+2)*(3+4))/5")
+    test("1+(2+(3+(4+5)))")
+    test("(1+(2*(3+(4*5))))")
+    test("(((((((1)))))))")    
+    test("1 + ((2 + 3) * (4 + 5))")
+    test("1 + (2 + 3 * (4 + (5 - 6)))")
     print("==== Test finished! ====\n")
 
 run_test()
@@ -139,6 +183,10 @@ while True:
     print('> ', end="")
     line = input()
     tokens = tokenize(line)
-    answer = evaluate_times_and_divide(tokens)
-    actual_answer = evaluate_plus_and_minus(answer)
-    print("answer = %f\n" % actual_answer)
+    evaluated_tokens, _ = evaluate_expression(tokens)
+    second_tokens = evaluate_times_and_divide(evaluated_tokens)
+    if second_tokens is None:
+        print("ZeroDivisionError\n")
+        continue
+    answer = evaluate_plus_and_minus(second_tokens)
+    print("answer = %f\n" % answer)
